1ba25b52145d70346d800b8e7fb6b0f29cdebd78
ronald1590 <apaza.ronald.c@gmail.com>
2022-12-05 11:45:26 -0500

    laravel version 4

---
+++ resources/views/pagLista.blade.php
@@ -0,0 +1,41 @@
+@extends('pagPlantilla')
+
+@section('titulo')
+    <h1 class="display-4">Página lista </h1>
+@endsection
+
+@section('seccion')
+    <h3>Lista </h3>
+    <table class="table">
+    <thead class="table-dark">
+    
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Código</th>
+      <th scope="col">Apellidos y Nombres</th>
+      <th scope="col">Fecha Nacimiento</th>
+      <th scope="col">Turno</th>
+      <th scope="col">Semestre</th>
+      <th scope="col">Estado</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($xAlumnos as $item)
+    <tr>
+      <th scope="row">{{$item->id}}</th>
+      <td>{{$item->codEst}}</td>
+      <td>{{$item->apeEst}},{{$item->nomEst}}</td>
+      <td>{{$item->fnaEst}}</td>
+      <td>{{$item->turMat}}</td>
+      <td>{{$item->semMat}}</td>
+      <td>{{$item->estMat}}</td>
+      <td>@mdo</td>
+    </tr>
+    @endforeach
+    </tbody>
+</table>
+    
+        
+    
+@endsection
